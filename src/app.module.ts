fc26d3d0728030b4b4e77da7d78d90d1ee8e8089
Hnat Liashenko <hnat.liashenko@bluepes.com>
2023-07-08 15:14:16 +0300

    config production and development .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
 import { SequelizeModule } from "@nestjs/sequelize";
 
 import { UsersModule } from "./users/users.module";
@@ -7,13 +8,16 @@ import { UsersModule } from "./users/users.module";
   controllers: [],
   providers: [],
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.${process.env.NODE_ENV}.env`,
+    }),
     SequelizeModule.forRoot({
       dialect: "postgres",
-      host: "localhost",
-      port: 5432,
-      username: "postgres",
-      password: "12345678",
-      database: "nestjs-backend-pg-docker",
+      host: process.env.POSTGRES_HOST,
+      port: Number(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
       models: [],
       autoLoadModels: true,
     }),
